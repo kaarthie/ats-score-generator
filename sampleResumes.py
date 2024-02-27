c23fe25528ef511a8017f018b390eeac8bb70fe0
kaarthie <karthironaldo989@gmail.com>
2024-02-27 19:04:02 +0530

    first commit

---
+++ sampleResumes.py
@@ -0,0 +1,60 @@
+resume1 = {
+    "Contact_information": {
+        "Name": "Ashutosh Kumar Mishra",
+        "email": "ash1234@gmail.com",
+        "phone": ""
+    },
+    "Resume_objective": "Dummy Objective",
+    "Working_experience": [
+        {
+            "Organization_name": "Punchh Tech India",
+            "role": "Developer",
+            "years_of_experience": 3,
+        },
+        {
+            "Organization_name": "IDEMIA",
+            "role": "SDE",
+            "years_of_experience": 2,
+        }
+    ],
+    "Projects": [
+        {
+            "Name": "My4cPoint, Garbanzo,Max & Ermas,Bd’s",
+            "description": "Domain: Online Food Ordering System.\nTechnology and Tool: React-native, Git, Github, Xcode, Android Studio, Sourcetree.",
+            "tech_stack": None
+        },
+        {
+            "Name": "RetailerShopperStop.com",
+            "description": "A portal is used for locating a list of available shops in a particular area according to the user search.",
+            "tech_stack": "Html,Css,JavaScript,PHP,MySQL"
+        },
+        {
+            "Name": "PayingGuest.com",
+            "description": "A portal is used for locating a list of available rooms on rent in a particular area according to the user search.",
+            "tech_stack": "PHP,MySQL"
+        }
+    ],
+    "Certifications": ["Python for Data Science by IBM", "Python Data Structure by Coursera"],
+    "Education": [
+        {
+            "Institution_name": "Kamla Nehru Institute of Technology, Sultanpur, Uttar Pradesh",
+            "Degree": "M.C.A",
+            "Marks": 85,
+            "Completion_year": "07/2018 - 06/2021"
+        },
+        {
+            "Institution_name": "T.E.R.I P.G College Ghazipur, Uttar Pradesh",
+            "Degree": "B.C.A",
+            "Marks": 85,
+            "Completion_year": "07/2015 - 05/2018"
+        }
+    ],
+    "Hard_skills": [
+        "React-Native", "JavaScript", "HTML", "CSS", "Python",
+        "MySQL", "GIT", "Data Structure", "Software Development", "ReactJS"
+    ],
+    "Soft_skills": []
+}
+
+resume2 = {"Contact_information": {"Name": "Gowtham Dharma E", "email": "gowthamdharma22@gmail.com", "phone": "8778166852"}, "Resume_objective": "I consider myself as a responsible and orderly person and looking\nforward for my first work experience.", "Working_experience": [], "Projects": [{"Name": "ONLINE BANKING SYSTEM", "description": "Online Banking System is to manage the details of Accounts, Internet\nBanking, Balance, and the domains we have used are\nHTML,CSS,XAMP.", "tech_stack": "HTML,CSS,XAMP"}, {"Name": "RESUME BUILDER", "description": "Resume builder is used to create a resume for free and helpful for\nStudents and the domains we have used are HTML,CSS,XAMP.", "tech_stack": "HTML,CSS,XAMP"}], "Certifications": ["Completed AICTE Cybersecurity \ninternships in Edu-Skills platform \nfrom 1/4/2022-16/5/2022", "Currently pursuing AICTE Aws \ncloud virtual internship in Edu- \nSkills platform from 3/8/2022", "Completed Introduction to Programming C course in\nNPTEL with Elite Silver badge.", "Completed Technical support fundamentals course in\nCOURSERA platform.", "Completed Python basics course in EDX platform.", "Completed Introduction to Data Science course in EDX\nplatform.", "Completed Image Processing Onramp course in MathWorks\nplatform."], "Education": [{"Institution_name": "M.Kumarasamy College Of Engineering Karur", "Degree": "B.E Computer Science and Engineering", "Marks": "8.7", "Completion_year": None}, {"Institution_name": "Arockiamatha Matric Higher Seconda ry School Trichy", "Degree": "HSC", "Marks": "71%", "Completion_year": "2019-2020"}, {"Institution_name": "Arockiamatha Matric Higher Seconda ry School Trichy", "Degree": "SSLC", "Marks": "83%", "Completion_year": "2017-2018"}], "Hard_skills": ["JAVA", "PYTHON", "SQL", "HTML&CSS"], "Soft_skills": ["Adaptability", "Communication", "Persistent problem-solver", "Strong attention to Detail", "Time management"]}
+resume3 = {"Contact_information": {"Name": "KANNAN D", "email": "kannand8787@gmail.com", "phone": "97373787087"}, "Resume_objective": "To work in a team in a smarter way and make a difference in whatever I do.", "Working_experience": [], "Projects": [{"Name": "ALUMINI RETRIEVAL SYSTEM - A website for managing all alumni details", "description": None, "tech_stack": None}], "Certifications": [], "Education": [{"Institution_name": "PSG college of Arts and Science", "Degree": "BSC COMPUTER TECHNOLOGY", "Marks": "81.14%", "Completion_year": "2020-Present"}, {"Institution_name": "Shri Ganga Higher Secondary School", "Degree": "HSC", "Marks": "86.6%", "Completion_year": "2020"}, {"Institution_name": "Shri Ganga Higher Secondary School", "Degree": "SSLC", "Marks": "94.8%", "Completion_year": "2018"}], "Hard_skills": ["C", "C++", "BASICS OF JAVA", "MYSQLDATABASE"], "Soft_skills": []}
